dc4185c5cbdc4d3d58a789074951cd12160f10b0
MrBaguettewastaken <marco@Tatino.lan>
2024-06-26 20:22:49 +0200

    tests

--- src/main/java/net/mcreator/levelup/init/LevelUpModItems.java
+++ src/main/java/net/mcreator/levelup/init/LevelUpModItems.java
@@ -10,12 +10,14 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.item.Item;
 
+import net.mcreator.levelup.item.TestItem;
 import net.mcreator.levelup.item.HelloItem;
 import net.mcreator.levelup.LevelUpMod;
 
 public class LevelUpModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, LevelUpMod.MODID);
 	public static final RegistryObject<Item> HELLO = REGISTRY.register("hello", () -> new HelloItem());
+	public static final RegistryObject<Item> TEST = REGISTRY.register("test", () -> new TestItem());
 	// Start of user code block custom items
 	// End of user code block custom items
 }
