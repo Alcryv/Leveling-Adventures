29fa5348674b198c85e6b5d46d0b387ab5e14e05
MrBaguettewastaken <marco@Tatino.lan>
2024-06-28 22:04:52 +0200

    ingot

--- src/main/java/net/mcreator/levelup/init/LevelUpModItems.java
+++ src/main/java/net/mcreator/levelup/init/LevelUpModItems.java
@@ -12,12 +12,14 @@ import net.minecraft.world.item.Item;
 
 import net.mcreator.levelup.item.AntimatterswordItem;
 import net.mcreator.levelup.item.AntimatteroreItem;
+import net.mcreator.levelup.item.AntimatteringotItem;
 import net.mcreator.levelup.LevelUpMod;
 
 public class LevelUpModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, LevelUpMod.MODID);
 	public static final RegistryObject<Item> ANTIMATTERSWORD = REGISTRY.register("antimattersword", () -> new AntimatterswordItem());
 	public static final RegistryObject<Item> ANTIMATTERORE = REGISTRY.register("antimatterore", () -> new AntimatteroreItem());
+	public static final RegistryObject<Item> ANTIMATTERINGOT = REGISTRY.register("antimatteringot", () -> new AntimatteringotItem());
 	// Start of user code block custom items
 	// End of user code block custom items
 }
